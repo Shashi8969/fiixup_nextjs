64c14832b9ac5bacac73c375dd8f20aa7433f8e8
SHASHI KUMAR <81017570+Shashi8969@users.noreply.github.com>
2026-03-29 17:05:57 +0530

    Update phone number placeholder in Contact component

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -132,7 +132,7 @@ export function Contact() {
                   id="contact-phone"
                   name="phone"
                   required
-                  placeholder="+91 98765 43210"
+                  placeholder="+91 8197459732"
                   autoComplete="tel"
                   className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-600 focus:outline-none"
                 />
